0e101ba7c2243fd09b72892758d71ee736102d66
Patrick Chai Tea <67674464+Squirrel10@users.noreply.github.com>
2021-02-24 13:37:33 -0800

    Add files via upload

--- Club/tests.py
+++ Club/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.contrib.auth.models  import User
+from .models import Resource, MeetingMinutes, Meetings, Event
+import datetime
 
-# Create your tests here.
+class MeetingsTest(TestCase):
+    def setUp(self):
+        self.type=Meetings(meetingtitle= 'test')
+
+
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'test')
+
+
+    def test_tablename(self):
+        self.assertEqual(str(Meetings._meta.db_table), 'meetings') 
+
+
+class ResourceTest(TestCase): 
+    def setUp(self): 
+        self.type=Meetings(meetingtitle= 'test_name')
+        self.user=User(username='user1')
+        self.resource=Resource(resourcename='Lenovo', resourcetype=self.type, user=self.user, reviewdate=datetime.date(2021,1,10), resourceprice=1200.99, resourceurl='http://www.lenovo.com', resourcedescription="lenovo laptop")   
+
+    
+    def test_string(self):
+        self.assertEqual(str(self.resource), 'Lenovo')
+
+    def test_discount(self):
+        disc = self.resource.resourceprice * .05
+        self.assertEqual(self.resource.discountAmount(), disc)
+
+
+    def test_discountAmount(self):
+        disc=self.resouce.resourceprice * (1 -.05)
+        self.assertEqual(self.resource.discountPrice(),disc)
+        
+
--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . models import Meetings, Resource, Event
+from django.urls import reverse_lazy 
+
 
 # Create your views here.
-def index (request): 
+def index(request): 
     return render(request, 'Club/index.html')
+
+def resources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'Club/resource.html', {'resource_list' : resource_list})
+
+def meetings(request, id): 
+    meeting=get_object_or_404(Meetings, pk=id)
+    return render(request, 'Club/meeting.html', {'meeting' : meeting})
+
